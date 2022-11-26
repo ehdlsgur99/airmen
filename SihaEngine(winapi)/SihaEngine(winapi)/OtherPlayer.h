f7f670c4a8a64bd51f5f6870952cb4f9d5a75d36
75xor <75520201+75xor@users.noreply.github.com>
2022-11-26 10:57:44 +0900

    Update OtherPlayer
    OtherPlayer클래스 코드추가

--- SihaEngine(winapi)/SihaEngine(winapi)/OtherPlayer.h
+++ SihaEngine(winapi)/SihaEngine(winapi)/OtherPlayer.h
@@ -3,6 +3,21 @@
 class OtherPlayer : public CSingleton<Player>
 {
 public:
+	int power;
+
+	OtherPlayer();
+	~OtherPlayer();
+
+	UserInfo getUserInfo();
+
+	int get2PlayerState();
+	void init();
+	void update();
+	void render();
+	void release();
+
+	void gravity(Tail* tail);
+public:
 	ePlayer state;
 	ePlayer nextState;
 	int hp;
@@ -11,10 +26,24 @@ public:
 	int nowMp;
 
 	std::string name;
-	int mappos = 0;
 	int JumpTime = 0;
 	int JumpCount = 0;
 	int JumpPower = 1000;
 
+
+public:
+	GameObject* player;
+	GameObject* smash;
+	PlayerBar* playerBar;
+	PlayerUI* playerUI;
+
+	int level;
+
+	bool isSmash;
+	bool isUI;
+	bool isAttack;
+	bool isJump;
+
+	UserInfo userInfo;
 };
 
